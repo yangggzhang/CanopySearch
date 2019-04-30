9b5043682f81286468449121884fe319566ee71d
zyyyyyyyyy <youngzh@umich.edu>
2019-04-30 18:13:48 -0400

    update msg

--- src/detector/scripts/live_detection.py
+++ src/detector/scripts/live_detection.py
@@ -12,17 +12,16 @@ from detection import CanopyDetector
 import numpy as np
 import random
 from move_canopy.srv import MovePos
-from move_canopy.msg import push
-from move_canopy.msg import confirm
+from move_canopy.msg import Push, Confirm
 
 class image_converter:
 
   def __init__(self):
     self.image_pub = rospy.Publisher("ros_image",Image)
-    self.arm_pub = rospy.Publisher('push', push)
+    self.arm_pub = rospy.Publisher('push', Push, queue_size=1)
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/camera/color/image_raw",Image,self.callback)
-    self.grab_sub = rospy.Subscriber("grab_status", confirm, self.confirm_callback)
+    self.grab_sub = rospy.Subscriber("grab_status", Confirm, self.confirm_callback)
     self.approx = None
     self.detector = CanopyDetector()
     self.grab_state = False # true finished
@@ -30,15 +29,23 @@ class image_converter:
     self.target_point = None
     self.RGB_start_point = None
     self.RGB_target_point = None
-    self.z = 0.4435
+    self.z = -0.4435
     self.P = [614.357421875, 0.0, 310.2319641113281, 0.0, 0.0, 614.494140625, 244.32691955566406, 0.0, 0.0, 0.0, 1.0, 0.0]
-    self.T = np.array([[-0.26258097, -0.96485145,  0.01062577,  0.02905107],
-                       [-0.0808903,  0.01103792, -0.99666189,  0.48671212], 
-                       [0.96151339, -0.26256397, -0.08094547, 0.21650044],
-                       [0.0,0.0,0.0,1.0]])
+    self.T = np.array([[-0.0213565 ,  0.99967616, -0.01383723, -0.01541872],
+                       [ 0.41591332,  0.02146953,  0.90915079, -0.54284319],
+                       [ 0.90915346,  0.01366119, -0.41623715, -0.55857797],
+                       [        0.0,         0.0,         0.0,         1.0]])
+                       
+    # self.T = np.array([[-0.26258097, -0.96485145,  0.01062577,  0.02905107],
+    #                    [-0.0808903,  0.01103792, -0.99666189,  0.48671212], 
+    #                    [0.96151339, -0.26256397, -0.08094547, 0.21650044],
+    #                    [0.0,0.0,0.0,1.0]])
+
+
+                       
   
   def confirm_callback(data):
-    self.grab_state = data.confirm
+    self.grab_state = data.Confirm
   
   def project(self, x, y):
     A = np.array([[self.P[0], self.P[1], -x], 
@@ -67,7 +74,7 @@ class image_converter:
 
     apple, count = self.detector.apple()
     cv2.drawContours(cv_image, apple, -1, (255, 0, 0), 3)
-    print(count)
+    # print(count)
 
     leaves, targets = self.detector.leaves()
     cv2.drawContours(cv_image, leaves, -1, ( 0, 255, 0), 3)
@@ -79,17 +86,17 @@ class image_converter:
       self.start_point = self.project(points[0], points[1])
       self.target_point = self.project(points[2], points[3])
 
-      msg = push()
+      msg = Push()
       msg.x0 = self.start_point[0]
       msg.y0 = self.start_point[1]
       msg.z0 = self.start_point[2]
       msg.x1 = self.target_point[0]
       msg.y1 = self.target_point[1]
       msg.z1 = self.target_point[2]
+      print(msg)
+      print("PUBLISH")
       self.arm_pub.publish(msg)
-
-      print("Point 1", self.start_point)
-      print("Point 2", self.target_point)
+      self.grab_state = True
       
       # self.progress = self.move_arm_client(self.start_point[0], self.start_point[1], self.start_point[2],
       #                                      self.target_point[0],self.target_point[1],self.target_point[2])
@@ -107,8 +114,8 @@ class image_converter:
       print(e)
 
 def main(args):
-  ic = image_converter()
   rospy.init_node('image_converter', anonymous=True)
+  ic = image_converter()
   try:
     rospy.spin()
   except KeyboardInterrupt:
--- src/move_canopy/scripts/single_move.py
+++ src/move_canopy/scripts/single_move.py
@@ -12,7 +12,7 @@ from analytic_ik import AnalyticInverseKinematics
 from rospy_tutorials.msg import Floats
 from std_msgs.msg import Empty, Bool
 from move_canopy.srv import MovePos, MovePosResponse
-from move_canopy.msg import push, confirm
+from move_canopy.msg import Push, Confirm
 
 ROSTOPIC_SET_ARM_JOINT = '/goal_dynamixel_position'
 ROSTOPIC_SET_PAN_JOINT = '/pan/command'
@@ -39,30 +39,30 @@ offset_z = 0.0
 class manipulator:
 
     def __init__(self):
-        self.confirm_pub = rospy.Publisher("grab_status",confirm)
-        self.target_sub = rospy.Subscriber("push", push, self.push_callback)
+        self.confirm_pub = rospy.Publisher("grab_status",Confirm)
+        self.target_sub = rospy.Subscriber("push", Push, self.push_callback)
 
-    def open_gripper(pub):
+    def open_gripper(self, pub):
         empty_msg = Empty()
         rospy.loginfo('Opening gripper')
         pub.publish(empty_msg)
 
-    def close_gripper(pub):
+    def close_gripper(self, pub):
         empty_msg = Empty()
         rospy.loginfo('Closing gripper')
         pub.publish(empty_msg)
 
-    def home_arm(pub):
+    def home_arm(self, pub):
         rospy.loginfo('Going to arm home pose')
         set_arm_joint(pub, np.zeros(5))
         rospy.sleep(1)
 
-    def set_arm_joint(pub, joint_target):
+    def set_arm_joint(self, pub, joint_target):
         joint_state = JointState()
         joint_state.position = tuple(joint_target)
         pub.publish(joint_state)
 
-    def linear_path(min_x, min_y, min_z , max_x, max_y, max_z, n_samples = 5):
+    def linear_path(self, min_x, min_y, min_z , max_x, max_y, max_z, n_samples = 5):
         x = np.linspace(min_x, max_x, n_samples)
         y = np.linspace(min_y, max_y, n_samples)
         xv, yv = np.meshgrid(x, y)
@@ -72,7 +72,7 @@ class manipulator:
         target_position = np.array([x,y,z]).T
         return target_position
 
-    def execute(target_position, gripper =  0.0, alpha = 0.0):
+    def execute(self, target_position, gripper =  0.0, alpha = 0.0):
         n, _ = target_position.shape
         for i in range(n):
 
@@ -87,7 +87,7 @@ class manipulator:
             else:
                 print('No IK Solution found for '+ str(target_position[i,:]))
 
-    def move_arm(x0, y0, z0, x1, y1, z1):
+    def move_arm(self, x0, y0, z0, x1, y1, z1):
         # go to x y z + threshold
         
         traj0 = linear_path( x0, y0 , z0, x1, y1, z1)
@@ -96,15 +96,16 @@ class manipulator:
         execute(traj1, gripper = GRIPPER_LOSE)
         time.sleep(0.1)
 
-    def push_callback(data):
+    def push_callback(self, data):
+        print("RECEIVE")
         move_arm(data.x0, data.y0, data.z0, data.x1, data.y1, data.z1)
-        msg = confirm()
+        msg = Confirm()
         msg.confirm = True
         self.confirm_pub.publish(msg)
 
 def main():
-    arm = manipulator()
     rospy.init_node('ARM', anonymous=True)
+    arm = manipulator()
     try:
         rospy.spin()
     except KeyboardInterrupt:
